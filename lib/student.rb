4b9aa3466a86d1abd77cf47f2f41cb0ae5fc0f09
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-24 01:32:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,17 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.create_table
+   sql = <<-SQL
+   CREATE TABLE IF NOT EXISTS students (
+     id INTEGER PRIMARY KEY,
+     name TEXT,
+     grade TEXT
+   )
+   SQL
+
+   DB[:conn].execute(sql)
+ end
 
 
 
